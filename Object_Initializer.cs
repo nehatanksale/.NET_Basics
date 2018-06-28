c2119d1bdd141ab3c48893108cbfabbd5f96a48e
Neha Tanksale <34652815+nehatanksale@users.noreply.github.com>
2018-06-28 18:12:23 +0530

    Create Object_Initializer.cs

---
+++ Object_Initializer.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Practice_Core
+{
+    class Program
+    {
+        String name;
+       
+        static void Main(string[] args)
+        {
+
+            var p = new Program()
+            {
+                name = "John";                     //initialsed here
+            };
+
+
+        }
+    }
+}
